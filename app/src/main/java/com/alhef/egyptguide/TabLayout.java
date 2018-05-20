01f328ef8490160cccc2c60e9ee516e54c13c2ec
medooo331982 <33475201+medooo331982@users.noreply.github.com>
2018-05-20 20:40:18 +0200

    Initial commit

---
+++ app/src/main/java/com/alhef/egyptguide/TabLayout.java
@@ -0,0 +1,11 @@
+package com.alhef.egyptguide;
+
+import android.support.v4.view.ViewPager;
+
+class TabLayout {
+    private ViewPager upWithViewPager;
+
+    public void setupWithViewPager(ViewPager upWithViewPager) {
+        this.upWithViewPager = upWithViewPager;
+    }
+}
